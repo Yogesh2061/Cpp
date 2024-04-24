b847b2683bb7d99e9b323f968526138d62da4315
Yogesh2061 <ybhandari461@gmail.com>
2024-04-24 16:22:02 +0545

    if-else conditions

--- 1.cpp
+++ 1.cpp
@@ -1,26 +1,32 @@
-//maximum between three number
-#include<iostream>
+// maximum between three number
+#include <iostream>
 using namespace std;
-int main(){
-int a,b,c;
-cin>>a>>b>>c;
-if(a>b){
-    if(a>c){
-        cout<<a<<endl;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a > b)
+    {
+        if (a > c)
+        {
+            cout << a << endl;
+        }
+        else
+        {
+            cout << c << endl;
+        }
     }
-    else{
-        cout<<c<<endl;
+    else
+    {
+        if (b > a)
+        {
+            cout << b << endl;
+        }
+        else
+        {
+            cout << c << endl;
+        }
     }
-}
-else {
-    if(b>a){
-        cout<<b<<endl;
-
-    }
-    else{
-        cout<<c<<endl;
-    }
-}
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Cpp
-Starting my C++ journey.
+Starting my c++ journey.
+
+starting by solving easy questions after knowing the basics of c++
+
+Qn.1)Find maximum from 3 given numbers using if else.
+Qn.2)check whether the number is odd or even
