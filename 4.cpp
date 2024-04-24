5ba31fc9dd376825f42b1f8701e77772c367ea22
Yogesh2061 <ybhandari461@gmail.com>
2024-04-24 23:52:55 +0545

    c++

--- 4.cpp
+++ 4.cpp
@@ -1,9 +1,10 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-int marks;
-cout<<"enter the marks: ";
-cin>>marks;
-marks>=32 ? cout<<"passed":cout<<"failed";
+int main()
+{
+    int marks;
+    cout << "enter the marks: ";
+    cin >> marks;
+    marks >= 32 ? cout << "passed" : cout << "failed";
     return 0;
 }
